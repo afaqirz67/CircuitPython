2dd68909a43ce0176a62b3507cd76f05d4813a4f
afaqirz67 <56890879+afaqirz67@users.noreply.github.com>
2020-10-19 01:06:40 -0400

    Update led_blink.py

--- led_blink.py
+++ led_blink.py
@@ -1,15 +1,1 @@
-import board
-import neopixel
-import time
 
-# make a neopixel object
-dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
-
-#this will run forever
-while True:
-    print("Make it green!")
-    dot.fill((0,255,0))
-    time.sleep(.5)
-    print("Make it red!")
-    dot.fill((255,0,0))
-    time.sleep(.5)
